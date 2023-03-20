4bba685ae7bece9de747de262c5da1a4fa52ac68
Caitlin <python.caitlin@gmail.com>
2023-03-20 15:50:53 -0600

    Solution for challengeTwo.

--- CodingChallenges.java
+++ CodingChallenges.java
@@ -29,3 +29,37 @@ class ChallengeOne{
 	}
 }
 
+class ChallengeTwo{
+	
+	public List<Integer> list = new ArrayList<>();	
+
+	public static void main(String... args) {
+		
+		Scanner in = new Scanner(System.in);
+		try{
+			System.out.print("Please enter a single positive integer: ");
+			int n = in.nextInt();
+			System.out.print("Please enter the number of multiples desired: ");
+			int max = in.nextInt();
+			in.close();
+
+			if(n < 0 || max < 0) throw new InputMismatchException();
+			
+			System.out.println("Multiples of " + n + ": " + arrayOfMultiple(n, max));
+		} catch(InputMismatchException e){
+			System.out.println("Invalid argument. Must be  a single, positive integer.");
+		}
+	}
+
+	static List<Integer> arrayOfMultiple(int n, int max) {
+		List<Integer> multiples = new ArrayList<>();
+		
+		for(int i = 1; i <= max; i++) {
+			multiples.add(n * i);
+		}
+
+		return multiples;
+		 	
+	}
+}
+
